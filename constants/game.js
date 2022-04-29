5a869acdce391c68967dee8e62ddd4d98ff8f7a7
Stephen Smart <stephendsmart@gmail.com>
2022-04-29 11:50:07 -0500

    Added board generation and started on other stuff
    Co-authored-by: Tammie Chung <tammiec@users.noreply.github.com>
    Co-authored-by: Olmo Buining <olmobuining@users.noreply.github.com>
    Co-authored-by: Bobby Wettig <bobbywettig@gmail.com>
    Co-authored-by: Chris Frazier <chrisFrazier77@users.noreply.github.com>

---
+++ constants/game.js
@@ -0,0 +1,14 @@
+const teams = {
+    red: 'red',
+    blue: 'blue'
+};
+
+const ships = {
+    b0: { name: "Battleship", length: 4 },
+    c0: { name: "Cruiser", length: 3 },
+    d0: { name: "Submarine", length: 3 },
+    e0: { name: "Destroyer", length: 2 },
+    a0: { name: "Carrier", length: 5 },
+}
+
+module.exports = { teams, ships };
